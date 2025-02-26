c3ee0eb1b4d0e808b6a8f6d1a93182577401405a
Peter <pmbugua276@gmail.com>
2025-02-26 23:44:10 +0300

    added the firebase file

--- firebaseConfig.jsx
+++ firebaseConfig.jsx
@@ -1,18 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+    apiKey: "AIzaSyD9b2jXzYJWDXRlMLKUcVD8P8qJc3EW5Tk",
+    authDomain: "fintech-8b1b2.firebaseapp.com",
+    projectId: "fintech-8b1b2",
+    storageBucket: "fintech-8b1b2.firebasestorage.app",
+    messagingSenderId: "1087916716163",
+    appId: "1:1087916716163:web:68c4a52247504311606bb6",
+    measurementId: "G-FXMXQ6BSZ7"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+const auth = getAuth(app); // Authentication
+const db = getFirestore(app); // Firestore Database
+const storage = getStorage(app); // Firebase Storage (for images, documents)
 
-export { auth, db };
+export { auth, db, storage };
