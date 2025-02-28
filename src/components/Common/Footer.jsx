28e3cf073a932f3e81abde60d3013a26695915a6
Peter <pmbugua276@gmail.com>
2025-02-28 14:23:27 +0300

    added footer

--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <div>
-      
-    </div>
-  )
+    <footer className="bg-gray-800 text-white py-4 text-center">
+      <p>&copy; {new Date().getFullYear()} BrandName. All rights reserved.</p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
