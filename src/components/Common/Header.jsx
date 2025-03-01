4a0fff26b328bc1bdec7b9ace51f106ba5929b08
Peter <pmbugua276@gmail.com>
2025-03-01 21:29:19 +0300

    added the help centre and the sell button on the header file

--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -13,9 +13,9 @@ function Header() {
         {/* Logo */}
         <div className="flex items-center space-x-2 text-4xl font-bold">
         <a href="#" className="flex items-center space-x-1 text-gray-800 hover:underline">
-  <span>Fintech</span>
-  <span className="bg-blue-600 text-white px-3 py-2 rounded-full text-lg font-extrabold">com</span>
-</a>
+          <span>Fintech</span>
+          <span className="bg-blue-600 text-white px-3 py-2 rounded-full text-lg font-extrabold">com</span>
+        </a>
 
 
 
