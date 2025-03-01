d63adb8b8bb2d2d1953dc483ee8267c64765e22d
Peter <pmbugua276@gmail.com>
2025-03-01 21:48:27 +0300

    added the help centre and the sell button on the header file

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,6 +4,48 @@ function Home() {
   return (
     <div>
       <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
+      <h1>Done</h1>
     </div>
   )
 }
