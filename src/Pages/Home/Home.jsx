406ab357e13938e71f97d61c692332e759f960d6
Peter <pmbugua276@gmail.com>
2025-03-01 23:02:43 +0300

    added some fonts and changed some parts of design

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -22,9 +22,9 @@ function Home() {
   ];
 
   return (
-    <div className="flex justify-center items-center p-4 bg-blue-600">
+    <div className="flex justify-center items-center p-2 bg-blue-600">
       {/* Container */}
-      <div className="w-[900px] bg-white rounded-lg shadow-md">
+      <div className="w-[1400px] bg-white rounded-lg shadow-md">
         {/* Top Section */}
         <div className="flex items-center border-b border-gray-300 p-2">
           {/* Dropdown Menu */}
