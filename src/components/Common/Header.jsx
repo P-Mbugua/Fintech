cb0c5594502e482c8f58992617392cb57bcde426
Peter <pmbugua276@gmail.com>
2025-03-01 21:15:37 +0300

    added the help centre and the sell button on the header file

--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -62,7 +62,7 @@ function Header() {
             <div className="absolute right-0 mt-2 w-40 bg-white shadow-md rounded-md opacity-0 group-hover:opacity-100 transition-all">
               <Link to="/profile" className="block px-4 py-2 hover:bg-gray-100">Profile</Link>
               <Link to="/Returns" className="block px-4 py-2 hover:bg-gray-100">Returns</Link>
-              <Link to="/logout" className="block px-4 py-2 hover:bg-gray-100">Logout</Link>
+              <Link to="/Track Orders" className="block px-4 py-2 hover:bg-gray-100">Track Orders</Link>
               <Link to="/logout" className="block px-4 py-2 hover:bg-gray-100">Logout</Link>
               <Link to="/logout" className="block px-4 py-2 hover:bg-gray-100">Logout</Link>
               <Link to="/logout" className="block px-4 py-2 hover:bg-gray-100">Logout</Link>
