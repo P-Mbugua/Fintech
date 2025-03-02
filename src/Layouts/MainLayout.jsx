0df82a0bcb8c5abbfa3f327a751cebbef1915d2d
Peter <pmbugua276@gmail.com>
2025-03-02 07:39:49 +0300

    removed the classname in the mainlayout in line 9 for efficiency

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -6,7 +6,7 @@ function MainLayout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className=" ">{children}</main>
+      <main>{children}</main>
       <Footer />
     </div>
   );
