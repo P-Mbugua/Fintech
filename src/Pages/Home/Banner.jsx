c57df3d09e7f6f13036bc26edf4bd0a6246193f2
Peter <pmbugua276@gmail.com>
2025-03-13 17:49:22 +0300

    added the logo on the rigister page

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,1 +1,11 @@
-rfc
+import React from 'react'
+
+function Banner() {
+  return (
+    <div>
+      This will be the banner space that I will be designing later
+    </div>
+  )
+}
+
+export default Banner
