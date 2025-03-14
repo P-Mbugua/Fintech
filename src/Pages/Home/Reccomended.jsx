d93a43255a2710356bd36456261c2c22a777b6a9
Peter <pmbugua276@gmail.com>
2025-03-14 18:01:28 +0300

    added some data to the recommended page

--- src/Pages/Home/Reccomended.jsx
+++ src/Pages/Home/Reccomended.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { ArrowRight, Star, ShoppingBag } from "lucide-react";
 
 const recommendedItems = [
@@ -30,13 +29,21 @@ const recommendedItems = [
   {
     id: 4,
     image: "https://via.placeholder.com/150",
+    title: "Fashion Lady Square Bag",
+    price: "KSh 399",
+    oldPrice: "KSh 609",
+    discount: "-34%",
+  },
+  {
+    id: 5,
+    image: "https://via.placeholder.com/150",
     title: "Fashion Women's Small Bag",
     price: "KSh 450",
     oldPrice: "KSh 809",
     discount: "-44%",
   },
   {
-    id: 5,
+    id: 6,
     image: "https://via.placeholder.com/150",
     title: "STY Women Small Cross Bag",
     price: "KSh 469",
@@ -62,10 +69,9 @@ function Recommended() {
         {/* Product List */}
         <div className="flex gap-4 overflow-x-auto scrollbar-hide">
           {recommendedItems.map((item) => (
-            <Link
-              to={`/product/${item.id}`}
+            <div
               key={item.id}
-              className="w-40 bg-gray-100 p-3 rounded-md shadow-md hover:shadow-xl hover:scale-105 transition duration-300 cursor-pointer"
+              className="w-40 bg-gray-100 p-3 rounded-md shadow-md hover:shadow-lg transition duration-300"
             >
               <img
                 src={item.image}
@@ -89,7 +95,7 @@ function Recommended() {
                   <Star className="w-4 h-4 text-gray-400" />
                 </div>
               </div>
-            </Link>
+            </div>
           ))}
         </div>
       </div>
