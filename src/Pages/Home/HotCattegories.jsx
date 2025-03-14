81c46eaf4530cee795f8f2be2733fd78c72c6d86
Peter <pmbugua276@gmail.com>
2025-03-14 18:23:38 +0300

    imported the page in the Main Page

--- src/Pages/Home/HotCattegories.jsx
+++ src/Pages/Home/HotCattegories.jsx
@@ -25,7 +25,7 @@ const categories = [
 
 const HotCategories = () => {
   return (
-    <div className="bg-red-600 p-4 rounded-lg">
+    <div className="bg-red-600 p-4 ">
       <h2 className="text-xl font-bold mb-4">Hot Category</h2>
       <div className="grid grid-cols-5 gap-4">
         {categories.map((category, index) => (
