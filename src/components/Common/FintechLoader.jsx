624efe7b29b1c4a6a4d88abcda8188714f74f415
Peter <pmbugua276@gmail.com>
2025-03-19 18:07:34 +0300

    added all the codes

---
+++ src/components/Common/FintechLoader.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const FintechLoader = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="relative w-16 h-16">
+        <div className="absolute w-full h-full border-4 border-red-600 border-t-transparent rounded-full animate-spin"></div>
+        <div className="absolute w-full h-full border-4 border-blue-600 border-r-transparent rounded-full animate-spin [animation-delay:-0.2s]"></div>
+        <div className="absolute w-full h-full border-4 border-green-600 border-b-transparent rounded-full animate-spin [animation-delay:-0.4s]"></div>
+      </div>
+    </div>
+  );
+};
+
+export default FintechLoader;
