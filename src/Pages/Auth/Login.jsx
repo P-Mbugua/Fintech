1530d1106cb1c098f6b0e19fdff4276562a72fbb
Peter <pmbugua276@gmail.com>
2025-03-20 07:01:47 +0300

    changed the main BG of the login page to gray-200 to mismatch them with the footer upper part

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -61,7 +61,7 @@ const Login = () => {
   };
 
   return (
-    <div className="flex flex-col items-center bg-gray-100 min-h-screen justify-center">
+    <div className="flex flex-col items-center bg-gray-200 min-h-screen justify-center">
       <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
         <div className="flex justify-center mb-4">
           <img src="https://img.kilimall.com/c/h5/login_bg.png?x-image-process=image/format,webp/resize,w_600" alt="Logo" className="h-12" />
